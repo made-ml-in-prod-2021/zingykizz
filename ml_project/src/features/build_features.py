85f98854a01b942c1f0281a60fd67b63039cc950
isk17 <isk17@europlan.ru>
2021-05-22 20:38:48 +0300

    удалено лишнее

--- ml_project/src/features/build_features.py
+++ ml_project/src/features/build_features.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from entities.feature_params import FeatureParams
+from src.entities.feature_params import FeatureParams
 
 
 def build_numerical_pipeline() -> Pipeline:
@@ -33,21 +34,19 @@ def build_transformer(params: FeatureParams) -> ColumnTransformer:
     return transformer
 
 
-def process_categorical_features(df: pd.DataFrame) -> pd.DataFrame:
-    categorical_pipeline = build_categorical_pipeline()
-    df.fillna("OTHER", inplace=True)
-    return pd.DataFrame(categorical_pipeline.fit_transform(df).toarray())
-
-
-def process_numerical_features(df: pd.DataFrame) -> pd.DataFrame:
-    pipeline = build_numerical_pipeline()
-    df.fillna(0, inplace=True)
-    return pd.DataFrame(pipeline.fit_transform(df))
+def serialize_transformer(transformer: ColumnTransformer, fout: str) -> None:
+    with open(fout, "wb") as f:
+        joblib.dump(transformer, f)
 
 
 def process_features(transformer: ColumnTransformer, df: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame(transformer.transform(df))
 
 
+def extract_features(df: pd.DataFrame, params: FeatureParams) -> pd.DataFrame:
+    return df[params.categorical_features + params.numerical_features]
+
+
 def extract_target(df: pd.DataFrame, params: FeatureParams) -> pd.Series:
     return df[params.target_col]
+
